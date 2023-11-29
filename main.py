a7a81ffbacdd10fd90c229d0df358b241c520c52
neuoni <rutnongneu@gmail.com>
2023-11-29 11:19:07 +0700

    update calculator

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ class Mycalculator:
         self.label = tk.Label(self.root, text="Hello DIP01", font=('Arial'))
         self.label.pack()
 
+        self.button = tk.Button(self.root, text="Clink here")
+        self.button.place(x=125, y=125)
+
         self.root.mainloop()
 
-Mycalculator
+Mycalculator()
